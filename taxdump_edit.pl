4a9b87d56a23419376952fc602e92dce2a1d845f
Guy Leonard <guy.leonard@gmail.com>
2018-08-23 14:54:13 +0100

    a few more lists, and some defaults set

--- taxdump_edit.pl
+++ taxdump_edit.pl
@@ -7,32 +7,33 @@ my $nodes = 'nodes.dmp';
 my $names = 'names.dmp';
 
 ## names.dmp specific
-my $new_name        = ''; # the name itself
-my $new_name_unique = ''; # the unique variant of this name if name not unique
-my $new_name_class       = ''; # acronym, anamorph, authority, blast name, common name, equivalent name, genbank acronym, genbank anamorph, genbank common name, genbank synonym, includes, in-part, misnomer, misspelling, scientific name, synonym, teleomorph, type material
+my $new_name        = '';    # the name itself
+my $new_name_unique = '';    # the unique variant of this name if name not unique
+my $new_name_class  = 'scientific name';    # see name class
 
 ## nodes.dmp specific
-my $parent_tax_id      = ''; # parent node id in GenBank taxonomy database
-my $rank               = ''; # rank of this node (superkingdom, kingdom, ...)
-my $embl_code          = ''; # locus-name prefix; not unique
-my $division_id   	   = ''; # see division.dmp file
-my $inherited_div_flag = ''; # (1 or 0) 1 if node inherits division from parent
-my $genetic_code_id    = ''; # see gencode.dmp file
-my $inherited_GC_flag  = ''; # (1 or 0) 1 if node inherits genetic code from parent
-my $mito_gen_code_id   = ''; # see gencode.dmp file
-my $inherited_MGC_flag = ''; # (1 or 0) 1 if node inherits mitochondrial gencode from parent
-my $gb_hidden_flag     = '0'; # (1 or 0) 1 if name is suppressed in GenBank entry lineage
-my $hidden_subtree_root_flag = '1'; # (1 or 0) 1 if this subtree has no sequence data yet
-my $comments           = ''; # free-text comments and citations
+my $parent_tax_id            = '';     # parent node id in GenBank taxonomy database
+my $rank                     = '';     # rank of this node (superkingdom, kingdom, ...)
+my $embl_code                = '';     # locus-name prefix; not unique
+my $division_id              = '';     # see division.dmp file
+my $inherited_div_flag       = '1';    # (1 or 0) 1 if node inherits division from parent
+my $genetic_code_id          = '1';     # see gencode.dmp file
+my $inherited_GC_flag        = '1';    # (1 or 0) 1 if node inherits genetic code from parent
+my $mito_gen_code_id         = '1';     # see gencode.dmp file
+my $inherited_MGC_flag       = '1';    # (1 or 0) 1 if node inherits mitochondrial gencode from parent
+my $gb_hidden_flag           = '1';    # (1 or 0) 1 if name is suppressed in GenBank entry lineage
+my $hidden_subtree_root_flag = '1';    # (1 or 0) 1 if this subtree has no sequence data yet
+my $comments                 = '';     # free-text comments and citations
 
 # flow
 my $largest_taxid = get_largest_tax_id($nodes);
 
 ## shared variables
-my $new_taxid = $largest_taxid + 1;
+# increased by a factor of 10 to it's length-1, to avoid conflicts
+my $new_taxid = $largest_taxid +
+  ( 10**( length($largest_taxid) - 1 ) );    
 
-print "$largest_taxid + 1 = $new_taxid\n";
-
+print "$largest_taxid + 10 ^ (" . length($largest_taxid) . "-1) = $new_taxid\n";
 
 # nodes.dmp examples
 #16	|	32011	|	genus	|		|	0	|	1	|	11	|	1	|	0	|	1	|	0	|	0	||
@@ -47,9 +48,9 @@ print "$largest_taxid + 1 = $new_taxid\n";
 #2304343	|	Odontostomatea	|		|	scientific name	|
 #2304349	|	Blattamonas Treitli et al. 2018	|		|	authority	|
 
-
 #############
 # subroutines
+#############
 
 # get the last line of the nodes or names file
 # return the value in the first tab column
@@ -66,7 +67,27 @@ sub get_largest_tax_id {
     return $lastline;
 }
 
-# ranks
+## name class
+# acronym
+# anamorph
+# authority
+# blast name
+# common name
+# equivalent name
+# genbank acronym
+# genbank anamorph
+# genbank common name
+# genbank synonym
+# includes
+# in-part
+# misnomer
+# misspelling
+# scientific name
+# synonym
+# teleomorph
+# type material
+
+## ranks
 # class
 # cohort
 # family
@@ -98,7 +119,7 @@ sub get_largest_tax_id {
 # tribe
 # varietas
 
-# division
+## division
 # 0	Bacteria
 # 1	Invertebrates
 # 2	Mammals
@@ -112,7 +133,7 @@ sub get_largest_tax_id {
 # 10	Vertebrates
 # 11	Environmental samples
 
-# genetic code
+## genetic code
 # 0	Unspecified
 # 1	Standard
 # 2	Vertebrate Mitochondrial
